14b215834216146150e598e2293baf92454f5a88
muskagup <muskagup@LIN49002346.corp.capgemini.com>
2021-02-22 13:54:14 +0530

    Path changed

--- Training/src/test/java/runner/SSrunner.java
+++ Training/src/test/java/runner/SSrunner.java
@@ -8,7 +8,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-	features= {"C:\\Users\\muskagup\\eclipse-workspace\\Training\\FeatureFile\\f.feature"}
+	features= {"FeatureFile/f.feature"}
 	,glue= {"stepdef","base"}
 	,tags= {"@tag4"}
 )
